10a670b52ddec223ff0ceefd8d1c8f5d04b9c901
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2025-07-28 17:07:36 -0400

    updates

--- src/components/footer/FooterV1.jsx
+++ src/components/footer/FooterV1.jsx
@@ -1,37 +1,11 @@
-import React, { useEffect } from 'react';
-import { HashLink as Link } from 'react-router-hash-link'
-import SocialShare from '../others/SocialShare';
-import { toast } from 'react-toastify';
+import React from 'react';
 
-// const LeadConnectorForm = () => {
-//   useEffect(() => {
-//     const script = document.createElement('script');
-//     script.src = 'https://link.msgsndr.com/js/form_embed.js';
-//     script.async = true;
-//     document.body.appendChild(script);
 
-//     return () => {
-//       // Clean up the script on component unmount
-//       document.body.removeChild(script);
-//     };
-//   }, []);
 
 const FooterV1 = () => {
-
-    const handleSearch = (event) => {
-        event.preventDefault()
-        event.target.reset()
-        toast.success("Thanks for your Email")
-    }
-
-
-
     return (
         <>
             <footer className="bg-dark text-light" style={{ backgroundImage: "url(/img/shape/5.png)" }}>
-                
-                    
-
                 <div className="footer-bottom bg-dark text-light text-center">
                     <div className="container">
                         <div className="row">
